30443684dead90ac0fc1087d3f4c610809f41da3
devilbash <marco.compostella@gmail.com>
2015-04-29 20:31:10 +0200

    prova

--- app/src/main/java/it/bestapp/paganino/Charts.java
+++ app/src/main/java/it/bestapp/paganino/Charts.java
@@ -1,51 +1,82 @@
 package it.bestapp.paganino;
 
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.webkit.WebView;
 
 import it.bestapp.paganino.adapter.TabsPagerAdapter;
+import it.bestapp.paganino.utility.db.bin.Busta;
 import it.bestapp.paganino.view.tabsliding.SlidingTabLayout;
 
 
 public class Charts extends ActionBarActivity
-                implements ActionBar.TabListener {
+                implements ActionBar.TabListener
+{
 
     private Toolbar toolbar;
     private ViewPager mViewPager;
     private SlidingTabLayout mSlidingTabLayout;
+   // private Busta busta;
+    private SlidingTabLayout tabs;
+
+
+    final Handler myHandler = new Handler();
+    private WebView wv;
+
+    TabsPagerAdapter mSectionsPagerAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.act_chart);
 
+
+        setContentView(R.layout.act_chart);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         if (toolbar != null) {
-            toolbar.setTitle("Paganino");
             setSupportActionBar(toolbar);
         }
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
+        tabs = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
+        tabs.setDistributeEvenly(true);
+        // Setting Custom Color for the Scroll bar indicator of the Tab View
+
+        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.tabsScrollColor);
+            }
+        });
+
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new TabsPagerAdapter(this, getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.viewpager);
-        mViewPager.setAdapter(new TabsPagerAdapter(this));
-        mSlidingTabLayout = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
-        mSlidingTabLayout.setTitleColor(Color.WHITE);
-        mSlidingTabLayout.setFittingChildren(true);
-        mSlidingTabLayout.setTabType(SlidingTabLayout.TabType.TEXT);
-        mSlidingTabLayout.setViewPager(mViewPager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+
+
+
+        tabs.setViewPager(mViewPager);
+
 
     }
 
 
     @Override
     public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-
+        mViewPager.setCurrentItem(tab.getPosition());
     }
 
     @Override
